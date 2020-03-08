b801108500dbc8aa60627160f385ebe7b7610d48
Camilo Caroso <camilitosamuel@hotmail.com>
2020-03-08 15:42:09 -0400

    Updates

---
+++ precache-manifest.4636d78a4db554b0b9a21928894fb726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03309f8e790d0bcb0b90e2c0b0263d3",
+    "url": "/burger-hw/index.html"
+  },
+  {
+    "revision": "a7bd32787839086417ba",
+    "url": "/burger-hw/static/css/main.07773988.chunk.css"
+  },
+  {
+    "revision": "fb7bdc0b93b230c6d2d3",
+    "url": "/burger-hw/static/js/2.9a3f2666.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/burger-hw/static/js/2.9a3f2666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bd32787839086417ba",
+    "url": "/burger-hw/static/js/main.70f8923d.chunk.js"
+  },
+  {
+    "revision": "aae91476dcd9551a1d22",
+    "url": "/burger-hw/static/js/runtime-main.a4367f26.js"
+  }
+]);
